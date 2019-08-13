9049d176e34f6cc0721b757d5063f4fb6fb10189
CybertekSchool <github17>
2019-08-13 13:10:37 -0400

    Java class

---
+++ Summer2019_Java/src/day01_FirstProgramming/PracticeShortCuts.java
@@ -0,0 +1,12 @@
+package day01_FirstProgramming;
+
+public class PracticeShortCuts {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Cybertek");
+			
+		
+	}
+	
+}
---
+++ Summer2019_Java/src/day01_FirstProgramming/classExample.java
@@ -0,0 +1,6 @@
+package day01_FirstProgramming;
+
+public class classExample {
+	
+
+}
