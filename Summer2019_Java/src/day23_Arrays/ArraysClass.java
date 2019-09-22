1a191f73a51b65cd50892807bc94f078c6dc59cd
Dina Urazalina <urazalinadina3@gmail.com>
2019-09-22 12:51:36 -0400

    changes from the text branch

--- Summer2019_Java/src/day23_Arrays/ArraysClass.java
+++ Summer2019_Java/src/day23_Arrays/ArraysClass.java
@@ -4,7 +4,9 @@ public class ArraysClass {
 	public static void main(String[] args) {
 
 		System.out.println("Git practice : Forking ... ");
-		
+
+		System.out.println("Git practice : Forking from local repo : IntelliJ  ");
+
 	}
 
 }
