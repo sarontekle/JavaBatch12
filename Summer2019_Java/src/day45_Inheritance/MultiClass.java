9660cf3098ea9b2e9d933ed71157f788ccbae529
cybertekschool <github@cybertekschool.com>
2019-10-29 13:10:58 -0400

    class

---
+++ Summer2019_Java/src/day45_Inheritance/MultiClass.java
@@ -0,0 +1,27 @@
+package day45_Inheritance;
+
+class A {
+	
+	public static void main(String[] args) {
+		System.out.println("default class A");
+	}
+	
+}
+
+public class MultiClass {
+
+	public static void main(String[] args) {
+		System.out.println("Main public class");
+	}
+	
+}
+
+
+
+class B{
+	
+	public static void main(String[] args) {
+		System.out.println("default class B");
+	}
+	
+}
