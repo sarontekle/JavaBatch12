3dff5ea35294b7ba689dc7152e1bcdd12bf689cd
cybertekschool <github@cybertekschool.com>
2019-11-01 17:08:30 -0400

    class

---
+++ Summer2019_Java/src/Day47_JavaReview/Constructors.java
@@ -0,0 +1,38 @@
+package Day47_JavaReview;
+
+class Mehmet{
+	
+	public Mehmet() {
+		System.out.println("Mehmet");
+	}
+	
+}
+
+class Resul extends Mehmet{
+	
+	public Resul() {
+		//supert()  // Mehmet
+		System.out.println("Resul");
+	}
+	
+}
+
+class Viktoria extends Resul {
+	
+	public Viktoria() {
+		super();
+		System.out.println("Viktoria");
+	}
+	
+}
+
+public class Constructors {
+	
+	public static void main(String[] args) {
+		
+		Viktoria obj = new Viktoria();
+		
+		
+	}
+
+}
---
+++ Summer2019_Java/src/Day47_JavaReview/Constructors2.java
@@ -0,0 +1,42 @@
+package Day47_JavaReview;
+
+class Cinthya{
+	
+	public Cinthya(int a) {
+		this(5.5);  // B
+		//this("a");
+		System.out.println("A");
+		// this(5.5);
+	}
+	
+	public Cinthya(double b) {
+		//this(5.5);  cannot call itself
+		 //this(10);  // cannot contain itself
+		System.out.println("B");
+	}
+	
+	public Cinthya(String str) {
+		System.out.println("C");
+	}	
+	/*
+	public Cinthya() {
+		
+	}
+	*/	
+}
+
+public class Constructors2 extends Cinthya {
+	
+	public Constructors2() {
+		super(5); //B
+	}
+	
+	public static void main(String[] args) {
+		Constructors2 obj = new Constructors2();  // C
+		
+		
+		
+	}
+	
+
+}
